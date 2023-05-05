d83cfba1ab1e06771ecf3d12c2e3097ef313dcac
Viktor Ivarsson <vicke.i93@gmail.com>
2023-05-05 22:43:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# zmk-config
+
+Heavily copied from
+[iamDrakkir/zmk-config](https://github.com/iamDrakkir/zmk-config)
+
+## Brain layout
+
+![image](https://github.com/viktorivarsson/zmk-config/blob/master/docs/export/brain.png)
+
+## Urchin layout
+
+![image](https://github.com/viktorivarsson/zmk-config/blob/master/docs/export/urchin.png)
---
+++ config/swe_keys_default.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+#pragma once
+
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
+#include <dt-bindings/zmk/keys.h>
+
+// Swedish definitions
+/*
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │ § │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ + │ ´ │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ Å │ ¨ │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │ A │ S │ D │ F │ G │ H │ J │ K │ L │ Ö │ Ä │ ' │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ < │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ - │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+/* Shifted symbols
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │ ½ │ ! │ " │ # │ ¤ │ % │ & │ / │ ( │ ) │ = │ ? │ ` │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │   │   │   │   │   │   │   │   │   │   │   │ ^ │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │   │   │   │   │   │   │   │   │   │   │   │ * │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ > │   │   │   │   │   │   │   │ ; │ : │ _ │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+/* AltGr symbols
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │   │   │ @ │ £ │ $ │ € │   │ { │ [ │ ] │ } │ \ │   │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │   │   │   │   │   │   │   │   │   │   │   │ ~ │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │   │   │   │   │   │   │   │   │   │   │   │   │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ | │   │   │   │   │   │   │ µ │   │   │   │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+
+#define SED_AE    LBKT             // Å
+#define SED_AA    SQT              // Ä
+#define SED_OO    SEMI             // Ö
+#define SED_EXCL  LS(N1)           // !
+#define SED_AT    RA(N2)           // @
+#define SED_HASH  LS(N3)           // #
+#define SED_DLLR  RA(N4)           // $
+#define SED_PRCNT LS(N5)           // %
+#define SED_AMPS  LS(N6)           // &
+#define SED_FSLH  LS(N7)           // /
+#define SED_STAR  LS(BSLH)         // *
+#define SED_CARET RBRC             // ^
+#define SED_LPAR  LS(N8)           // (
+#define SED_RPAR  LS(N9)           // )
+#define SED_LBKT  RA(N8)           // [
+#define SED_RBKT  RA(N9)           // ]
+#define SED_LBRC  RA(N7)           // {
+#define SED_RBRC  RA(N0)           // }
+#define SED_PLUS  MINUS            // +
+#define SED_MINUS FSLH             // -
+#define SED_EQUAL LS(N0)           // =
+#define SED_QMARK LS(SED_PLUS)      // ?
+#define SED_UNDER LS(SED_MINUS)     // _
+#define SED_DQT   LS(N2)           // "
+#define SED_SQT   BSLH             // '
+#define SED_GRAVE PLUS             // ´
+#define SED_TILDE RA(RBKT)         // ~
+#define SED_LT    NON_US_BACKSLASH // <
+#define SED_GT    LS(SED_LT)        // >
+#define SED_BSLH  RA(SED_PLUS)      // (backslash)
+#define SED_PIPE  RA(SED_LT)        // |
+
